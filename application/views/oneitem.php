c23156a91b796d292cede6905d0b073b8fc693eb
Gisu Kim <sksmszhdzk@gmail.com>
2017-11-30 14:00:23 -0800

    close# 48

--- application/views/oneitem.php
+++
@@ -1,5 +0,0 @@
-<tr>
-        <td>{id}</td>
-        <td>{name}</td>
-        <td>{status}</td>
-</tr>
--- application/views/oneitemx.php
+++
@@ -1,6 +0,0 @@
-<tr>
-    <!-- MODIFY the next line -->
-<td><a href="/mtce/edit/{id}"><input type="button" value="{id}"/></a></td>
-<td>{name}</td>
-<td>{status}</td>
-</tr>
